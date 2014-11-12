b44fd3176beef989a9e547e657485aa163f6d5a2
Stephen Dixon <sjdixon2@gmail.com>
2014-11-11 18:21:33 -0700

    Added vendor folder to gitignore Got propel "get" method working. Renamed namespace of DebateClub classes.

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <html>
 	<p> Hello World! </p>
 	<?php
-		echo "Hello World Noob!";
+
+// setup the autoloading
+require_once 'vendor/autoload.php';
+
+// setup Propel
+require_once 'generated-conf/config.php';
+		$q = new TournamentQuery();
+		$firstTournament = $q->findPK(1);
+		echo $firstTournament->getName();
 	?>
 </html>
